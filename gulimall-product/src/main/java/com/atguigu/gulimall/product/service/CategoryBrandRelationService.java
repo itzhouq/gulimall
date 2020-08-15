3bbeca78a0fc1f1b24048d3eeb58c68fbd516e80
itzhouq <itzhouq@163.com>
2020-08-15 20:42:21 +0800

    商品-获取分类关联的品牌

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/CategoryBrandRelationService.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/CategoryBrandRelationService.java
@@ -1,9 +1,11 @@
 package com.atguigu.gulimall.product.service;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.atguigu.common.utils.PageUtils;
 import com.atguigu.gulimall.product.entity.CategoryBrandRelationEntity;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,5 +24,12 @@ public interface CategoryBrandRelationService extends IService<CategoryBrandRela
     void updateBrand(Long brandId, String name);
 
     void updateCategory(Long catId, String name);
+
+    /**
+     * 获取分类关联的品牌
+     * @param catId 分类Id
+     * @return
+     */
+    List<BrandEntity> getBrandsByCatId(Long catId);
 }
 
