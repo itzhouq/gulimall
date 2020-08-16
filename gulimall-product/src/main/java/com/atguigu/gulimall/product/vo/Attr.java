1ee16646e8ba8b798bc7526e02efb3246d90a98c
itzhouq <itzhouq@163.com>
2020-08-16 16:13:33 +0800

    商品-新增商品接口准备

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/Attr.java
@@ -0,0 +1,22 @@
+/**
+  * Copyright 2020 bejson.com 
+  */
+package com.atguigu.gulimall.product.vo;
+
+import lombok.Data;
+
+/**
+ * Auto-generated: 2020-08-16 10:49:53
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+@Data
+public class Attr {
+
+    private Long attrId;
+    private String attrName;
+    private String attrValue;
+
+
+}
---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/Bounds.java
@@ -0,0 +1,22 @@
+/**
+  * Copyright 2020 bejson.com 
+  */
+package com.atguigu.gulimall.product.vo;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * Auto-generated: 2020-08-16 10:49:53
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+@Data
+public class Bounds {
+
+    private BigDecimal buyBounds;
+    private BigDecimal growBounds;
+
+}
