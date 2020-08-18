1bcc704c7cf7a51e32153f002f5e4c500ff471c3
itzhouq <itzhouq@163.com>
2020-08-19 00:26:14 +0800

    仓储服务-整合ware服务

--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/GulimallWareApplication.java
@@ -3,9 +3,13 @@ package com.atguigu.gulimall.ware;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@EnableTransactionManagement
 @MapperScan("com.atguigu.gulimall.ware.dao")
+@EnableDiscoveryClient
+@SpringBootApplication
 public class GulimallWareApplication {
     public static void main(String[] args) {
         SpringApplication.run(GulimallWareApplication.class, args);
